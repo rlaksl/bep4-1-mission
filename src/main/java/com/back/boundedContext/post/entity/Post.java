abba8cfe3ec678bc61694e7bd935a3d08a3e82e8
rlaksl <eehiuh@gmail.com>
2026-09-17 22:02:03 +0900

    0006

--- src/main/java/com/back/boundedContext/post/entity/Post.java
+++ src/main/java/com/back/boundedContext/post/entity/Post.java
@@ -2,6 +2,8 @@ package com.back.boundedContext.post.entity;
 
 import com.back.boundedContext.member.entity.Member;
 import com.back.global.jpa.entity.BaseIdAndTime;
+import com.back.shared.post.dto.PostCommentDto;
+import com.back.shared.post.event.PostCommentCreatedEvent;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
@@ -41,7 +43,7 @@ public class Post extends BaseIdAndTime {
 
         comments.add(postComment);
 
-        author.increaseActivityScore(1);
+        publishEvent(new PostCommentCreatedEvent(new PostCommentDto(postComment)));
 
         return postComment;
     }
