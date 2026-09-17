bf4f595118e41d519b0c79537f1995e6fad69f7d
rlaksl <eehiuh@gmail.com>
2026-09-17 20:57:44 +0900

    0003

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -4,9 +4,15 @@ import com.back.jpa.entity.BaseIdAndTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
@@ -20,9 +26,24 @@ public class Post extends BaseIdAndTime {
     @Column(columnDefinition = "LONGTEXT")
     private String content;
 
+    @OneToMany(mappedBy = "post", cascade = {PERSIST, REMOVE}, orphanRemoval = true)
+    private List<PostComment> comments = new ArrayList<>();
+
     public Post(Member author, String title, String content) {
         this.author = author;
         this.title = title;
         this.content = content;
+    }
+
+    public PostComment addComment(Member author, String content) {
+        PostComment postComment = new PostComment(this, author, content);
+
+        comments.add(postComment);
+
+        return postComment;
+    }
+
+    public boolean hasComments() {
+        return !comments.isEmpty();
     }
 }
--- src/main/java/com/back/service/PostService.java
+++ src/main/java/com/back/service/PostService.java
@@ -6,17 +6,24 @@ import com.back.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
 
+    public long count() {
+        return postRepository.count();
+    }
+
     public Post write(Member author, String title, String content) {
         Post post = new Post(author, title, content);
+
         return postRepository.save(post);
     }
 
-    public long count() {
-        return postRepository.count();
+    public Optional<Post> findById(int id) {
+        return postRepository.findById(id);
     }
 }
