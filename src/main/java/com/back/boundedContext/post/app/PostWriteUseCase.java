91ab7f69670b5c7b4eb3d6204549913a046c5b04
rlaksl <eehiuh@gmail.com>
2026-09-18 11:46:49 +0900

    0014

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -1,6 +1,6 @@
 package com.back.boundedContext.post.app;
 
-import com.back.boundedContext.member.app.MemberFacade;
+import com.back.shared.member.out.MemberApiClient;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
-    private final MemberFacade memberFacade;
+    private final MemberApiClient memberApiClient;
 
     public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
@@ -27,8 +27,7 @@ public class PostWriteUseCase {
                 )
         );
 
-        // MemberFacade 직접 호출
-        String randomSecureTip = memberFacade.getRandomSecureTip();
+        String randomSecureTip = memberApiClient.getRandomSecureTip();
 
         return new RsData<>(
                 "201-1", "%d번 글이 생성되었습니다. 보안 팁 : %s"
