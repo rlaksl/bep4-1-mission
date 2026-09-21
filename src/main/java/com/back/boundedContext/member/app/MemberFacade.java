dc53ea257e327cf40edae7728f8bc4dd0ab20bdc
rlaksl <eehiuh@gmail.com>
2026-09-21 11:17:08 +0900

    0029

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -1,8 +1,6 @@
 package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
-import com.back.boundedContext.member.domain.MemberPolicy;
-import com.back.boundedContext.member.out.MemberRepository;
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,14 +11,9 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class MemberFacade {
-    private final MemberRepository memberRepository;
+    private final MemberSupport memberSupport;
     private final MemberJoinUseCase memberJoinUseCase;
-    private final MemberPolicy memberPolicy;
-
-    @Transactional(readOnly = true)
-    public long count() {
-        return memberRepository.count();
-    }
+    private final MemberGetRandomSecureTipUseCase memberGetRandomSecureTipUseCase;
 
     // RsData<Member>
     @Transactional
@@ -29,18 +22,21 @@ public class MemberFacade {
     }
 
     @Transactional(readOnly = true)
+    public long count() {
+        return memberSupport.count();
+    }
+
+    public String getRandomSecureTip() {
+        return memberGetRandomSecureTipUseCase.getRandomSecureTip();
+    }
+
+    @Transactional(readOnly = true)
     public Optional<Member> findByUsername(String username) {
-        return memberRepository.findByUsername(username);
+        return memberSupport.findByUsername(username);
     }
 
     @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
-        return memberRepository.findById(id);
-    }
-
-    // MemberPolicy에서 가져옴
-    public String getRandomSecureTip() {
-        return "비밀번호의 유효기간은 %d일 입니다."
-                .formatted(memberPolicy.getNeedToChangePasswordDays());
+        return memberSupport.findById(id);
     }
 }
