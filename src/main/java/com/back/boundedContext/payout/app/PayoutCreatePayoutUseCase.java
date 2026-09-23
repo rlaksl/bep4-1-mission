b7aa4714d2afd40fcae71870090599f60de1ea3e
rlaksl <eehiuh@gmail.com>
2026-09-23 11:47:20 +0900

    0048

--- src/main/java/com/back/boundedContext/payout/app/PayoutCreatePayoutUseCase.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutCreatePayoutUseCase.java
@@ -14,8 +14,8 @@ public class PayoutCreatePayoutUseCase {
     private final PayoutMemberRepository payoutMemberRepository;
     private final PayoutRepository payoutRepository;
 
-    public Payout createPayout(PayoutMemberDto payee) {
-        PayoutMember _payee = payoutMemberRepository.getReferenceById(payee.getId());
+    public Payout createPayout(int payeeId) {
+        PayoutMember _payee = payoutMemberRepository.getReferenceById(payeeId);
 
 
         Payout payout = payoutRepository.save(new Payout(_payee));
